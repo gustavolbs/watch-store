56f6816c89d9f2d5c5db6cfed47c4129546f5d86
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-11 23:52:43 -0300

    add cart tests

--- components/Cart.vue
+++ components/Cart.vue
@@ -1,10 +1,15 @@
 <template>
   <div
     class="fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300"
+    :class="{ hidden: !isOpen }"
   >
     <div class="flex items-center justify-between">
       <h3 class="text-2xl font-medium text-gray-700">Your cart</h3>
-      <button class="text-gray-600 focus:outline-none">
+      <button
+        data-testid="close-button"
+        class="text-gray-600 focus:outline-none"
+        @click="close"
+      >
         <svg
           class="h-5 w-5"
           fill="none"
@@ -19,22 +24,14 @@
       </button>
     </div>
     <hr class="my-3" />
-    <cart-item />
-    <div class="mt-8">
-      <form class="flex items-center justify-center">
-        <input
-          class="form-input w-48"
-          type="text"
-          placeholder="Add promocode"
-        />
-        <button
-          class="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-        >
-          <span>Apply</span>
-        </button>
-      </form>
-    </div>
+    <cart-item
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+    <h3 v-if="!hasProducts">Cart is empty</h3>
     <a
+      v-else
       class="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
     >
       <span>Checkout</span>
@@ -58,5 +55,25 @@ import CartItem from '@/components/CartItem';
 export default {
   name: 'CartComponent',
   components: { CartItem },
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+    products: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    hasProducts() {
+      return this.products.length > 0;
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
 };
 </script>
